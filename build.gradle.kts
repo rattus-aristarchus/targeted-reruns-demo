0e618fdfb3314d03c21a1c2ad2ab39178f0e8bda
rattus-aristarchus <lankin.m.a@gmail.com>
2026-09-21 06:44:07 +0300

    workflow update, java version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,8 +22,8 @@ allure {
 }
 
 tasks.withType(JavaCompile::class) {
-    sourceCompatibility = "${JavaVersion.VERSION_1_8}"
-    targetCompatibility = "${JavaVersion.VERSION_1_8}"
+    sourceCompatibility = "${JavaVersion.VERSION_25}"
+    targetCompatibility = "${JavaVersion.VERSION_25}"
     options.encoding = "UTF-8"
 }
 
